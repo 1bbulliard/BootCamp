440f1e1908c067c5ddc782724c43614cafb0b612
bbulliard <bbulliard@gmail.com>
2021-11-29 10:06:39 -0600

    Initial Commit

---
+++ Shared/BootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootCampApp.swift
+//  Shared
+//
+//  Created by Bob Bulliard on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct BootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
